67d6b47460f5197e9611cde001e2469465823c3e
ART <pakkawat.kh@gmail.com>
2021-11-26 16:07:01 +0700

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,41 +1,43 @@
-
 const routes = [
   {
-    path: '/',
-    component:() =>import('pages/loading.vue')
+    path: "/",
+    component: () => import("pages/loading.vue")
   },
   {
-    path: '/mapview',
-    component:() =>import('pages/mapview.vue')
+    path: "/mapview",
+    component: () => import("pages/mapview.vue")
   },
   {
-    path: '/listview',
-    component:() =>import('pages/listview.vue')
+    path: "/listview",
+    component: () => import("pages/listview.vue")
   },
   {
-    path: '/report',
-    component:() =>import('pages/report.vue')
-    
+    path: "/report",
+    component: () => import("pages/report.vue")
   },
   {
-    path: '/trend',
-    component:() =>import('pages/trend.vue')
+    path: "/trend",
+    component: () => import("pages/trend.vue")
   },
   {
-    path: '/document',
-    component:() =>import('pages/document.vue')
+    path: "/document",
+    component: () => import("pages/document.vue")
   },
   {
-    path: '/detail',
-    component:() =>import('pages/detail.vue')
+    path: "/detail",
+    component: () => import("pages/detail.vue")
+  },
+  {
+    path: "/test",
+    component: () => import("pages/test.vue")
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '*',
-    component: () => import('pages/Error404.vue')
+    path: "*",
+    component: () => import("pages/Error404.vue")
   }
-]
+];
 
-export default routes
+export default routes;
